13c6c39808e0d066d45aae88784cd1cef86ec7cc
Meena9 <mehta.meenamin123@gmail.com>
2021-03-15 00:44:03 +0530

    add changes

--- src/app/side-nav-bar/side-nav-bar.component.ts
+++ src/app/side-nav-bar/side-nav-bar.component.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from "@angular/core";
-import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState,
+} from "@angular/cdk/layout";
 import { Observable } from "rxjs";
 import { map, shareReplay } from "rxjs/operators";
+import { ActivatedRoute } from "@angular/router";
 @Component({
   selector: "app-side-nav-bar",
   templateUrl: "./side-nav-bar.component.html",
   styleUrls: ["./side-nav-bar.component.css"],
 })
 export class SideNavBarComponent implements OnInit {
-  isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(
-      map((result) => result.matches),
-      shareReplay()
-    );
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  isHandset = false;
 
-  ngOnInit() {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.breakpointObserver
+      .observe(Breakpoints.Handset)
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.isHandset = true;
+        } else {
+          this.isHandset = false;
+        }
+      });
+  }
 }
